65c9d1b3eeb22f19d1340777e132339b30b91941
Ruturaj Molawade <molawaderuturaj@gmail.com>
2025-12-04 22:01:58 -0800

    add day4 part 2

--- day4/PrintingDepartmentPart1.java
+++ day4/PrintingDepartmentPart1.java
@@ -5,7 +5,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
-public class PrintingDepartment {
+public class PrintingDepartmentPart1 {
 
     static char[][] grid;
 
